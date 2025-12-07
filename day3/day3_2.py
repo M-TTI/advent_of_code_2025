9d6de5fea6fd5a2259c89da0d8b76580a17fe3c8
M-TTI <martin.bonetti.dev@gmail.com>
2025-12-07 11:05:06 +0100

    still not working

--- day3/day3_2.py
+++ day3/day3_2.py
@@ -1,22 +1,43 @@
-output = 0
+total_joltage = 0
+break_check = False
 
-with open('day3_test.input', 'r') as str_input:
+with open('day3.input', 'r') as str_input:
     for line in str_input:
+        line = line.strip()
         new_number = ''
         exclude_last_digits = 11
         max_index = 0
 
         while exclude_last_digits > 0:
-            line_cpy = line.strip()[max_index:]
-            print(f'line_cpy: {line_cpy}')
-            max_digit = 0
+            if max_index + exclude_last_digits == len(line) - 1:
+                for i in range(max_index, len(line)):
+                    new_number += str(line[i])
+                max_index = len(line)
+                break_check = True
+                break
+            max_digit = 1
 
-            for i in range(len(line_cpy)):
+            line_cpy = line[:-exclude_last_digits]
+            for i in range(max_index, len(line_cpy)):
                 if max_digit < int(line_cpy[i]):
                     max_digit = int(line_cpy[i])
-                    max_index = i
+                    max_index = i + 1
 
-                new_number += str(max_digit)
-                exclude_last_digits -= 1
+            new_number += str(max_digit)
+            print(f'line_cpy: {line_cpy}, max_index: {max_index}, exclude_last_digits: {exclude_last_digits}, new_number: {new_number}')
+            exclude_last_digits -= 1
+        if not break_check:
+            line_cpy = line
+            for i in range(max_index, len(line_cpy)):
+                if max_digit < int(line_cpy[i]):
+                    max_digit = int(line_cpy[i])
 
+            new_number += str(max_digit)
+
+        print(f'line_cpy: {line_cpy}, max_index: {max_index}, exclude_last_digits: {exclude_last_digits}, new_number: {new_number}')
+
+        assert len(new_number) == 12
         print(new_number)
+        total_joltage += int(new_number)
+
+print(total_joltage)
