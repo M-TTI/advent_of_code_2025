d93fcdf106c0db542981595da11ffe3bdb44940a
Martin Bonetti <martin.bonetti@ynov.com>
2025-12-09 17:47:16 +0100

    done day4 part 1 and 2

---
+++ day4/day4_1.py
@@ -0,0 +1,105 @@
+def is_forkliftable(current_roll_index, current_row, upper_row = None, lower_row = None):
+	places_to_check = []
+	roll_counter = 0
+
+	if current_roll_index == 0:
+		if upper_row == None:
+			places_to_check.extend([
+				current_row[current_roll_index + 1], 
+				lower_row[current_roll_index], 
+				lower_row[current_roll_index + 1],
+			])
+		elif lower_row == None:
+			places_to_check.extend([
+				upper_row[current_roll_index + 1], 
+				upper_row[current_roll_index],
+				current_row[current_roll_index + 1], 
+			])
+		else:
+			places_to_check.extend([
+				upper_row[current_roll_index + 1], 
+				upper_row[current_roll_index],
+				current_row[current_roll_index + 1],
+				lower_row[current_roll_index], 
+				lower_row[current_roll_index + 1],
+			])
+
+	elif current_roll_index == len(current_row) - 1:
+		if upper_row == None:
+			places_to_check.extend([
+				current_row[current_roll_index - 1], 
+				lower_row[current_roll_index - 1], 
+				lower_row[current_roll_index],
+			])
+		elif lower_row == None:
+			places_to_check.extend([
+				upper_row[current_roll_index], 
+				upper_row[current_roll_index - 1],
+				current_row[current_roll_index - 1], 
+			])
+		else:
+			places_to_check.extend([
+				upper_row[current_roll_index], 
+				upper_row[current_roll_index - 1],
+				current_row[current_roll_index - 1],
+				lower_row[current_roll_index - 1], 
+				lower_row[current_roll_index],
+			])
+
+	else:
+		if upper_row == None:
+			places_to_check.extend([
+				current_row[current_roll_index - 1],
+				lower_row[current_roll_index - 1],
+				lower_row[current_roll_index],
+				lower_row[current_roll_index + 1],
+				current_row[current_roll_index + 1],
+			])
+		elif lower_row == None:
+			places_to_check.extend([
+				current_row[current_roll_index + 1],
+				upper_row[current_roll_index + 1],
+				upper_row[current_roll_index], 
+				upper_row[current_roll_index - 1],
+				current_row[current_roll_index - 1], 
+			])
+		else:
+			places_to_check.extend([
+				upper_row[current_roll_index + 1],
+				upper_row[current_roll_index], 
+				upper_row[current_roll_index - 1],
+				current_row[current_roll_index - 1],
+				lower_row[current_roll_index - 1], 
+				lower_row[current_roll_index],
+				lower_row[current_roll_index + 1],
+				current_row[current_roll_index + 1]
+			])
+
+	for roll in places_to_check:
+		# print(f'current_roll_index: {current_roll_index}, roll: {roll}')
+		roll_counter += 1 if roll == '@' else 0
+
+	return roll_counter < 4
+
+
+count_forkliftable = 0
+grid = []
+
+with open('day4_test.input', 'r') as file:
+	for line in file:
+		grid.append(line)
+
+	upper_row = None
+	lower_row = grid[1]
+	for row_index in range(len(grid)):
+		row = grid[row_index]
+		lower_row = grid[row_index + 1] if row_index < len(row) - 1 else None
+
+		for current_index in range(len(row)):
+			if row[current_index] == '@':
+				if is_forkliftable(current_index, row, upper_row, lower_row):
+					count_forkliftable += 1
+
+		upper_row = row
+
+print(count_forkliftable)
